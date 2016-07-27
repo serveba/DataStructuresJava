14e4893da025a36cdb81d8c820b54cc8ca0a315a
Sergio Velasco <serveba@prot-os.com>
2016-07-27 20:03:31 +0200

    Queue formatting

--- src/main/java/com/serveba/adt/Queue.java
+++ src/main/java/com/serveba/adt/Queue.java
@@ -2,35 +2,35 @@ package com.serveba.adt;
 
 public class Queue {
 
-  private LinkedList list;
+	private LinkedList list;
 
-  public Queue() {
-    list = new LinkedList();
-  }
+	public Queue() {
+		list = new LinkedList();
+	}
 
-  public Object peek() {
-    return list.get(0);
-  }
+	public Object peek() {
+		return list.get(0);
+	}
 
-  public void enqueue(Object data) {
-    list.add(data);
-  }
+	public void enqueue(Object data) {
+		list.add(data);
+	}
 
-  public boolean dequeue() {
-    return list.removeInitial();
-  }
+	public boolean dequeue() {
+		return list.removeInitial();
+	}
 
-  public int size() {
-    return list == null ? 0 : list.size();
-  }
-  
-  public boolean isEmpty() {
-	  return size() == 0;
-  }
+	public int size() {
+		return list == null ? 0 : list.size();
+	}
 
-  @Override
-  public String toString() {
-      return list == null ? "" : list.toString();
-  }
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+
+	@Override
+	public String toString() {
+		return list == null ? "" : list.toString();
+	}
 
 }
